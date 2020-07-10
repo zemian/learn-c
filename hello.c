049c1672b9f4d57abe910c906ddd7d8385a1e987
Zemian Deng <zemian.deng@oracle.com>
2020-07-10 13:04:59 -0400

    Add basic c lang exercises

---
+++ hello.c
@@ -0,0 +1,17 @@
+// https://www.tutorialspoint.com/cprogramming/index.htm
+
+/*
+C programming is a general-purpose, procedural, imperative computer programming
+language developed in 1972 by Dennis M. Ritchie
+*/
+
+// C keywords: https://www.tutorialspoint.com/cprogramming/c_basic_syntax.htm
+
+#include <stdio.h>
+
+int main() {
+   /* my first program in C */
+   printf("Hello, World! \n");
+
+   return 0;
+}
---
+++ typedef.c
@@ -0,0 +1,11 @@
+// https://www.tutorialspoint.com/cprogramming/c_typedef.htm
+
+/*
+The typedef allows you to redefine type into new name.
+ex: "unsigned char" => BYTE
+The onvention is to use UPPER_CASE for new types
+
+Example:
+    typedef unsigned char BYTE;
+    BYTE b1, b2;
+*/
